0366bf2549e7d9f9899edc33218ce4d718375ab7
manuel.carrera.al@gmail.com <manuel@nulldata.com>
2016-10-18 18:10:14 -0500

    se removieron comentarios

--- routes/index.js
+++ routes/index.js
@@ -3,67 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	//createKeys();
-  	//res.render('index', { title: 'Express' });
 
-  	var db = req.db;
+    var db = req.db;
     var collection = db.get('phoneNumbers');
 
-	getMethods(collection);
+  getMethods(collection);
 
-	collection.col.aggregate(
+  collection.col.aggregate(
     [
       { "$group": {
         _id: "$state",
         total: {$sum:1}
       }},
       {
-      	"$sort":
-      	{
-      		total:-1
-      	}
+        "$sort":
+        {
+          total:-1
+        }
       }
     ],
     function(err,result) {
       console.log( JSON.stringify( result, undefined, 4 ) );
-    	res.render('index', {
-			registers : JSON.stringify(result)
-		});
+      res.render('index', {
+      registers : JSON.stringify(result)
+    });
     }
   );
-
-	/*var arr = collection.aggregate([
-		          {$match: {}}
-		        , {$group:
-		            {_id: '$state', total: {$sum: 1} }
-		          }
-		      ]);	*/
-		
-
-    /*collection.find({},{},function(e,docs){
-
-    	console.log("ENCONTRO: " + docs[100].state);   
-    	res.render('index', {
-			registers : docs
-		});
-    });*/
 });
-
-
 
 function getMethods(obj)
 {
     var res = [];
     for(var m in obj) {
         if(typeof obj[m] == "function") {
-        	console.log("FUNC; " + m);
-            //res.push(m)
+          console.log("FUNC; " + m);
         }
     }
-    //return res;
 }
-
-
-
 
 module.exports = router;
