afda3ce9adcd5ddf7113cf8ff08d3b797fbba215
manuel.carrera.al@gmail.com <manuel@nulldata.com>
2016-10-20 19:05:05 -0500

    Se agregaron los datos de INEGI de penetracion de uso de equipos moviles y se integraron al grafico. Actualizacion de instrucciones

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var csv = require("fast-csv");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
     var db = req.db;
     var collection = db.get('phoneNumbers');
 
-  getMethods(collection);
+  //getMethods(collection);
 
   collection.col.aggregate(
     [
@@ -23,10 +25,11 @@ router.get('/', function(req, res, next) {
       }
     ],
     function(err,result) {
-      console.log( JSON.stringify( result, undefined, 4 ) );
-      res.render('index', {
-      registers : JSON.stringify(result)
-    });
+
+      createKeys(res, result);
+
+      //console.log( JSON.stringify( result, undefined, 4 ) );
+      
     }
   );
 });
@@ -39,6 +42,52 @@ function getMethods(obj)
           console.log("FUNC; " + m);
         }
     }
+}
+
+function createKeys(res, states) {
+  var obj = {};
+  csv
+   .fromPath("public/inegi.csv")
+   .on("data", function(data){
+    var mInfo = {};
+    mInfo.pib = data[1];
+    mInfo.pt = data[2];
+    obj[data[0] + ""] = mInfo;
+     
+     console.log("DATA: " + JSON.stringify(obj[data[0]]) + ""); 
+    // claves[data[2]] = data[1];
+     //console.log("Normal: " +claves[data[2]]);
+     console.log("DATA: " + data);  
+
+   })
+   .on("end", function(){
+    // console.log("FINAL: "  + JSON.stringify(obj));
+    for (var prop in states) {
+      console.log("Estado: " + states[prop]._id);
+      if(obj[states[prop]._id]){
+        states[prop].pib = obj[states[prop]._id].pib;
+        states[prop].pt = obj[states[prop]._id].pt;
+      } else {
+        states[prop].pib = 0;
+        states[prop].pt = 0;
+      }
+      
+      //prop.pbi =        
+    } 
+     console.log("FINAL: "  + JSON.stringify(states));
+
+  res.render('index', {
+        registers : JSON.stringify(states)
+    });
+    //console.log("FINAL: "  + JSON.stringify(states));
+   
+    /*for (var prop in claves) {
+      console.log("ARR: " +prop + " DESC: " + claves[prop]);
+    } */
+       console.log("done");
+
+      // insertDataBase(req);
+   });
 }
 
 module.exports = router;
